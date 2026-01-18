982ae7e8f86fac095191e1efb1029239dd640850
tpural <tapuralewski@gmail.com>
2026-01-17 23:38:04 -0600

    fix(deps): update eslint-plugin-next and patch config for compatibility

--- packages/config/eslint/nextjs.js
+++ packages/config/eslint/nextjs.js
@@ -1,10 +1,14 @@
-/** @type {import('eslint').Linter.Config} */
+const nextPlugin = require('@next/eslint-plugin-next');
+
+// Patch to remove "name" property incompatible with ESLint 8
+const recommended = { ...nextPlugin.configs.recommended };
+delete recommended.name;
+
 module.exports = {
-  extends: [
-    require.resolve('./index.js'),
-    'plugin:@next/next/recommended',
-  ],
+  extends: [require.resolve('./index.js')],
+  plugins: ['@next/next'],
   rules: {
+    ...recommended.rules,
     '@next/next/no-html-link-for-pages': 'error',
   },
 };
